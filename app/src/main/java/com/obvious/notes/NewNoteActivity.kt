7a664910edfbdc3686faed8da87c25a168a978bb
vijayalakshmi <vijayalakshmi.in@yni.global>
2020-03-05 15:39:39 +0530

    Resolved unresolved reference

--- app/src/main/java/com/obvious/notes/NewNoteActivity.kt
+++ app/src/main/java/com/obvious/notes/NewNoteActivity.kt
@@ -14,8 +14,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import kotlinx.android.synthetic.main.note_item.*
 
 class NewNoteActivity : AppCompatActivity() {
     companion object {
